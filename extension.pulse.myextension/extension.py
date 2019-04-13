df2a9bb4a3df74c89398f063672028c0635a4486
PulseMedia <info@pulse-player.tv>
2019-04-13 11:40:05 +0200

    Add onPlay Event and add the "tool" parameter to the Extension class

--- extension.pulse.myextension/extension.py
+++ extension.pulse.myextension/extension.py
@@ -7,21 +7,31 @@ import xbmcaddon
 addon = xbmcaddon.Addon('extension.pulse.myextension') #ExtensionID
 initApiCall = addon.getSetting('apiCall') #Read out settings from Settings.xml with the respective id (id = "apiCall")
 
-pulseapi = None #PulseApi caching
+pulseapi = None
+Tool = None
 
 def Call(call, *args): #Wrapper Function
     global pulseapi
     argParsed = call
     for arg in args:
-        argParsed = argParsed + "/" + str(arg)  
+        argParsed = argParsed + "/" + str(arg)
     pulseapi(argParsed)
 
 class Extension:
 
-    def __init__(self, api):
+    def __init__(self, api, tools):
         global pulseapi #Making global variable "pulseapi" accessible
+        global Tool #Making global variable "Tool" accessible
+
         pulseapi = api #Set PulseApi to the global variable (for caching)
-        
+        Tool = tools #Set Tools to the global variable (for caching)
+
         Call(initApiCall) #Run ApiCall which has been set in Settings.xml (and use Wrapper function)
         #Alternative can also be used:
         #pulseapi(initApiCall)
+
+    def onPlay(self, url): #Will be called if Pulse try to start playing an Media which contains the extension Badge ([myextension])
+        pass
+
+
+    # FOR MORE INFORMATION VISIT: https://www.pulse-player.tv/extension
